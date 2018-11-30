a94f1f785019cfeba50dc340fcca07b89594fb24
Johnathon Wood <jaywood128@gmail.com>
2018-11-30 16:38:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
+
+  attr_accessor :equilateral
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    binding.pry
+    if ((a == b) && (b == c) && (c==a))
+       self.kind == :equilateral
+
+    end
+  end
+
+  class TriangleError < StandardError
+  end
   # write code here
 end
